b8db6879793242086b71ffe7357f7d26d481ae05
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-10-19 02:11:47 -0400

    create instances of Player and Computer. Use their method to create game

---
+++ main.rb
@@ -0,0 +1,37 @@
+require './computer'
+require './player'
+
+# create Player's instances. We have two players for the game.
+Player1 = Player.new('Alice', 3, nil)
+Player2 = Player.new('Alex', 3, nil)
+
+# create computer instance for the game, we call it Magi
+Magi = Computer.new(Player1, Player2)
+
+# assign Magi to both players as their computers
+Player1.computer = Magi
+Player2.computer = Magi
+
+# store method called on instance (aka behaviour) from Magi as an array so we can iterate the sequance
+run_game = [
+  -> { Magi.ask_question },
+  -> { Magi.current_player.get_answer },
+  -> { Magi.current_player.game_message },
+  -> { Magi.current_player.update_score },
+  -> { Magi.print_score },
+  -> { Magi.switch_turn }
+]
+
+loop do
+  run_game.each do |behaviour|
+    behaviour.call
+
+    # logic of ending the game
+    next unless Magi.player1.score <= 0 || Magi.player2.score <= 0
+
+    puts '------- Game Over --------'
+    # print winner info
+    Magi.get_winner
+    return
+  end
+end
